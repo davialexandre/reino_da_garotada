291ce0fbc4dee302330ed4e103fd0d418b2aac7e
lutz <USER@DOMAIN>
2009-11-29 23:42:26 +0000

    git-svn-id: svn://davialexandre.com.br/davialexandre/reinodagarotada/trunk@41 536801ca-0ed6-4cf8-8ea2-ec1ace6b8836

--- controllers/usuarios/create.php
+++ controllers/usuarios/create.php
@@ -3,11 +3,11 @@
 	include 'includes/usuarios.inc';
 	$form = include 'forms/usuarios_form.frm';
 	$message = '';
-	if(isset($_POST['nome_usuarios'])) {
+	if(isset($_POST['nome'])) {
 		fill_form($form);
 		if(validate_form($form)) {
 			$usuarios = form_to_simple_array($form);
-			if(create_usuarios($usuarios)) {
+			if(save_usuarios($usuarios)) {
 				set_message('Usuário criado! <a href="?r=usuarios/view/'. db_last_insert_id() .'" >Clique aqui para visualizar</a>');
 				redireciona_para('usuarios/edit/'.db_last_insert_id());
 			} else {
