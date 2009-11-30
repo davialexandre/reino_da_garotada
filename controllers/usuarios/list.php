8b386deedd59c26e5c31ee24cd0d18f58e5e17b9
lutz <USER@DOMAIN>
2009-11-30 02:19:47 +0000

    git-svn-id: svn://davialexandre.com.br/davialexandre/reinodagarotada/trunk@50 536801ca-0ed6-4cf8-8ea2-ec1ace6b8836

---
+++ controllers/usuarios/list.php
@@ -0,0 +1,13 @@
+<?php
+	include_once 'includes/usuarios.inc';
+	
+	$total_usuarios = db_fetch_column(db_query('SELECT COUNT(id) FROM usuario'));
+	
+	$pagina_atual = isset($_GET['page']) ? $_GET['page'] - 1 : 0;
+	$from = $pagina_atual * 4;
+	$usuarios = load_usuarios($from, 4);
+	
+	$pager = create_pager($total_usuarios, 4);
+	
+	exibe_view(array('usuario' => $usuarios, 'pager' => $pager));
+?>
