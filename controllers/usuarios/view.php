5ed3d8400a53ff27ba9ffbe02ee1e09e436436d0
lutz <USER@DOMAIN>
2009-11-30 19:15:07 +0000

    git-svn-id: svn://davialexandre.com.br/davialexandre/reinodagarotada/trunk@74 536801ca-0ed6-4cf8-8ea2-ec1ace6b8836

---
+++ controllers/usuarios/view.php
@@ -0,0 +1,23 @@
+<?php
+	include 'includes/posts.inc';
+	
+	$form_comentario = include_once 'forms/comment_form.frm';
+	
+	if(isset($_POST['id_post'])) {
+		fill_form($form_comentario);
+		if(validate_form($form_comentario)) {
+			$comentario = form_to_simple_array($form_comentario);
+			if(db_save_record('comentario', $comentario)) {
+				set_message('Comentário adicionado!');
+			} else {
+				set_message('Não foi possível adicionar o comentário' . get_last_error());
+			}
+		}
+	}
+	
+	$post = load_post($params[0]);
+	$post['corpo'] = newline_to_br($post['corpo']);
+	$categorias = load_categorias_post($post['id']);
+	$comentarios = load_post_comments($post['id']);
+	exibe_view(array('post' => $post, 'categorias' => $categorias, 'comentarios' => $comentarios, 'form_comentario' => $form_comentario, 'message' => get_message()));
+?>
