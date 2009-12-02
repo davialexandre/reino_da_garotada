48d1fd5b5cc9f369e0ce4d86cb0a1b58c9e0f9c1
davialexandre <davi@davialexandre.com.br>
2009-12-02 15:02:58 +0000

    git-svn-id: svn://davialexandre.com.br/davialexandre/reinodagarotada/trunk@100 536801ca-0ed6-4cf8-8ea2-ec1ace6b8836

---
+++ controllers/route/add_alias.php
@@ -0,0 +1,16 @@
+<?php
+	
+	$form = include_once('forms/add_alias_form.frm');
+	
+	if(isset($_POST['route'])) {
+		fill_form($form);
+		if(validate_form($form)) {
+			$route_alias = form_to_simple_array($form);
+			db_save_record('route_alias', $route_alias);
+		}
+	}
+	
+	$aliases = db_fetch_all(db_query('SELECT * FROM route_alias'));
+	
+	exibe_view(array('form' => $form, 'aliases' => $aliases), 'admin');
+?>
---
+++ controllers/route/delete_alias.php
@@ -0,0 +1,11 @@
+<?php
+	if(isset($params[0]) && is_numeric($params[0])) {
+		if(db_query('DELETE FROM route_alias WHERE id = ?', array($params[0]))) {
+			redireciona_para('route/add_alias');
+		} else {
+			echo get_last_error();
+		}
+	} else {
+		mostra_erro_http(404, 'Página não encontrada');
+	}
+?>
