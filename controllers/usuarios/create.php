c30701acb34984227de6a5d0bdc5124f645de232
lutz <USER@DOMAIN>
2009-12-02 17:11:33 +0000

    git-svn-id: svn://davialexandre.com.br/davialexandre/reinodagarotada/trunk@106 536801ca-0ed6-4cf8-8ea2-ec1ace6b8836

--- controllers/usuarios/create.php
+++ controllers/usuarios/create.php
@@ -3,15 +3,15 @@
 	include 'includes/usuarios.inc';
 	$form = include 'forms/usuarios_form.frm';
 	$message = '';
-	if(isset($_POST['titulo'])) {
+	if(isset($_POST['nome'])) {
 		fill_form($form);
 		if(validate_form($form)) {
-			$eventos = form_to_simple_array($form);
-			if(save_eventos($eventos)) {
-				set_message('Evento adicionado! <a href="?r=eventos/view/'. db_last_insert_id() .'" >Clique aqui para visualizar</a>');
-				redireciona_para('eventos/edit/'.db_last_insert_id());
+			$usuarios = form_to_simple_array($form);
+			if(save_usuarios($usuarios)) {
+				set_message('Usuário criado com sucesso! <a href="?r=usuarios/view/'. db_last_insert_id() .'" >Clique aqui para visualizar</a>');
+				redireciona_para('usuarios/edit/'.db_last_insert_id());
 			} else {
-				$message = 'Não foi possível adicionar o evento. Tente novamente.';
+				$message = 'Não foi possível criar o usuário. Tente novamente.';
 			}
 		}
 	}
