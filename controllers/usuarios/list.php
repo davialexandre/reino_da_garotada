4577d5bdf5a54f0ac1a5cda8fcf1161a510a4368
lutz <USER@DOMAIN>
2009-12-02 00:39:11 +0000

    correção / eventos sobreposto
    git-svn-id: svn://davialexandre.com.br/davialexandre/reinodagarotada/trunk@84 536801ca-0ed6-4cf8-8ea2-ec1ace6b8836

--- controllers/usuarios/list.php
+++ controllers/usuarios/list.php
@@ -5,7 +5,7 @@
 	
 	$pagina_atual = isset($_GET['page']) ? $_GET['page'] - 1 : 0;
 	$from = $pagina_atual * 4;
-	$usuarios = load_eventos($from, 4);
+	$usuarios = load_usuarios($from, 4);
 	
 	$pager = create_pager($total_usuarios, 4);
 	
