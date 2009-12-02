04d0993f9bbdcec6049b489efbdc3f50038bc170
lutz <USER@DOMAIN>
2009-12-02 00:38:31 +0000

    git-svn-id: svn://davialexandre.com.br/davialexandre/reinodagarotada/trunk@83 536801ca-0ed6-4cf8-8ea2-ec1ace6b8836

--- settings.php
+++ settings.php
@@ -1,17 +1,17 @@
 <?php
 	global $db_config, $site_name;
-	/*$db_config = array(
+	$db_config = array(
 		'servidor' 	=> 'mysql.davialexandre.com.br',
 		'username' 	=> 'davialexandre08',
 		'password' 	=> 'reino123',
 		'dbname'	=> 'davialexandre08',
-	);*/
-	$db_config = array(
+	);
+	/*$db_config = array(
 		'servidor' 	=> 'localhost',
 		'username' 	=> 'root',
 		'password' 	=> '',
 		'dbname'	=> 'reino_da_garotada',
-	);
+	);*/
 	
 	$site_name = 'Reino da Garotada';
 ?>
