ff7b2800aceaabf2f92d5c8acc7a156f9e4dc083
lutz <USER@DOMAIN>
2009-12-02 00:37:53 +0000

    git-svn-id: svn://davialexandre.com.br/davialexandre/reinodagarotada/trunk@82 536801ca-0ed6-4cf8-8ea2-ec1ace6b8836

--- controllers/usuarios/list.php
+++ controllers/usuarios/list.php
@@ -1,13 +1,13 @@
 <?php
-	include_once 'includes/eventos.inc';
+	include_once 'includes/usuarios.inc';
 	
-	$total_eventos = db_fetch_column(db_query('SELECT COUNT(id) FROM eventos'));
+	$total_usuarios = db_fetch_column(db_query('SELECT COUNT(id) FROM usuarios'));
 	
 	$pagina_atual = isset($_GET['page']) ? $_GET['page'] - 1 : 0;
 	$from = $pagina_atual * 4;
-	$eventos = load_eventos($from, 4);
+	$usuarios = load_eventos($from, 4);
 	
-	$pager = create_pager($total_eventos, 4);
+	$pager = create_pager($total_usuarios, 4);
 	
-	exibe_view(array('eventos' => $eventos, 'pager' => $pager), 'admin');
+	exibe_view(array('usuarios' => $usuarios, 'pager' => $pager));
 ?>
