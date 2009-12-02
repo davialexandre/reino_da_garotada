2cfedc2cdf1ab1e2d5cf42daf7d90f5c9426c3d7
lutz <USER@DOMAIN>
2009-12-02 01:04:17 +0000

    git-svn-id: svn://davialexandre.com.br/davialexandre/reinodagarotada/trunk@97 536801ca-0ed6-4cf8-8ea2-ec1ace6b8836

--- controllers/usuarios/list.php
+++ controllers/usuarios/list.php
@@ -1,7 +1,7 @@
 <?php
 	include_once 'includes/usuarios.inc';
 	
-	$total_usuarios = db_fetch_column(db_query('SELECT COUNT(id) FROM usuarios'));
+	$total_usuarios = db_fetch_column(db_query('SELECT COUNT(id) FROM usuario'));
 	
 	$pagina_atual = isset($_GET['page']) ? $_GET['page'] - 1 : 0;
 	$from = $pagina_atual * 4;
