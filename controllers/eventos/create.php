604410b6a79312881535b08762b313506a505a4d
lutz <USER@DOMAIN>
2009-12-02 19:52:12 +0000

    git-svn-id: svn://davialexandre.com.br/davialexandre/reinodagarotada/trunk@128 536801ca-0ed6-4cf8-8ea2-ec1ace6b8836

--- controllers/eventos/create.php
+++ controllers/eventos/create.php
@@ -8,7 +8,7 @@
 		if(validate_form($form)) {
 			$eventos = form_to_simple_array($form);
 			if(save_eventos($eventos)) {
-				set_message('Evento criado com sucesso! <a href="?r=usuarios/view/'. db_last_insert_id() .'" >Clique aqui para visualizar</a>');
+				set_message('Evento criado com sucesso! <a href="?r=eventos/view/'. db_last_insert_id() .'" >Clique aqui para visualizar</a>');
 				redireciona_para('eventos/edit/'.db_last_insert_id());
 			} else {
 				$message = 'Não foi possível adicionar o evento. Tente novamente.';
